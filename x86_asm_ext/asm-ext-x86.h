6337020d2d5711fa6d5088e6b55ab4621d3a8e48
mrodenkirch <mrodenkirch>
2020-05-26 14:30:44 +0000

    Changes so that it builds cleanly when linked with GMP regardless of platform
    git-svn-id: svn://svn.code.sf.net/p/mtsieve/svn@27 bb85bfea-1d01-4b18-b6d8-510d50b5ba24

--- x86_asm_ext/asm-ext-x86.h
+++ x86_asm_ext/asm-ext-x86.h
@@ -12,66 +12,75 @@
    mode and reset it with mod_fini when done.
 */
 
+// Handle the possibility that mp_limb_t and uint64_t are different typedefs.
+// We don't use mp_limb_t here, but the same parameters passed to these calls
+// are also passed to GMP functions so the types must be compatible.
+#ifdef WIN32
+   #define uns_64 unsigned long long
+#else
+   #define uns_64 unsigned long
+#endif
+
 extern "C" {
    // Assembly language functions in mulmod128.S
-   extern void mulmod128(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N, const uint64_t inv);
-   extern void mulmod128_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod128(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N, const uns_64 inv);
+   extern void mulmod128_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in sqrmod128.S
-   extern void sqrmod128(const uint64_t *R, const uint64_t *N, const uint64_t inv);
-   extern void sqrmod128_proth0(const uint64_t *R, const uint64_t *N, const uint64_t inv);
-   extern void sqrmod128_proth1(const uint64_t *R, const uint64_t *N);
+   extern void sqrmod128(const uns_64 *R, const uns_64 *N, const uns_64 inv);
+   extern void sqrmod128_proth0(const uns_64 *R, const uns_64 *N, const uns_64 inv);
+   extern void sqrmod128_proth1(const uns_64 *R, const uns_64 *N);
 
    // Assembly language functions in mulmod192.S
-   extern void mulmod192(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N, const uint64_t inv);
-   extern void mulmod192_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod192_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod192(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N, const uns_64 inv);
+   extern void mulmod192_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod192_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in sqrmod192.S
-   extern void sqrmod192(const uint64_t *R, const uint64_t *N, const uint64_t inv);
-   extern void sqrmod192_proth0(const uint64_t *R, const uint64_t *N);
-   extern void sqrmod192_proth1(const uint64_t *R, const uint64_t *N);
+   extern void sqrmod192(const uns_64 *R, const uns_64 *N, const uns_64 inv);
+   extern void sqrmod192_proth0(const uns_64 *R, const uns_64 *N);
+   extern void sqrmod192_proth1(const uns_64 *R, const uns_64 *N);
 
    // Assembly language functions in mulmod256.S
-   extern void mulmod256_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod256_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod256_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod256_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in sqrmod256.S
-   extern void sqrmod256_proth0(const uint64_t *R, const uint64_t *N);
-   extern void sqrmod256_proth1(const uint64_t *R, const uint64_t *N);
+   extern void sqrmod256_proth0(const uns_64 *R, const uns_64 *N);
+   extern void sqrmod256_proth1(const uns_64 *R, const uns_64 *N);
 
    // Assembly language functions in m320.S
-   extern void mulmod320_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod320_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod320_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod320_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m384.S
-   extern void mulmod384_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod384_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod384_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod384_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m448.S
-   extern void mulmod448_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod448_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod448_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod448_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m512.S
-   extern void mulmod512_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod512_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod512_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod512_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m576.S
-   extern void mulmod576_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod576_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod576_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod576_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m640.S
-   extern void mulmod640_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod640_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod640_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod640_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m704.S
-   extern void mulmod704_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod704_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod704_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod704_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in m768.S
-   extern void mulmod768_proth0(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
-   extern void mulmod768_proth1(const uint64_t *R, const uint64_t *A, const uint64_t *B, const uint64_t *N);
+   extern void mulmod768_proth0(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
+   extern void mulmod768_proth1(const uns_64 *R, const uns_64 *A, const uns_64 *B, const uns_64 *N);
 
    // Assembly language functions in redc.S
-   extern void redc_proth0(mp_limb_t * R, const mp_limb_t *N, size_t nn);
+   extern void redc_proth0(uns_64 *R, const uns_64 *N, size_t nn);
 }
