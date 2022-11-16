4c1d025d5a7b8b7135cee8edc4d154bf76137842
Seth Troisi <sethtroisi@google.com>
2022-11-16 13:37:27 -0800

    HashTable: Refactor logic, add comments

--- core/HashTable.h
+++ core/HashTable.h
@@ -45,7 +45,7 @@ public:
          htable[slot] = j;
       else
       {
-         olist[j] = (htable[slot] ^ HASH_MASK1);
+         olist[j] = htable[slot];
          htable[slot] = (j | HASH_MASK1);
       }
    };
@@ -53,38 +53,53 @@ public:
    inline uint32_t Lookup(uint64_t bj)
    {
       uint32_t slot;
-      uint16_t elt;
+      uint16_t elt, elt_low;
 
       slot = bj & hsize_minus1;
       elt = htable[slot];
+      elt_low = elt & HASH_MASK2;
 
-      // Could check elt == empty_slot to avoid checking BJ64
+      //if (elt == empty_slot)
+      //   return HASH_NOT_FOUND;
 
-      if (BJ64[elt & HASH_MASK2] == bj)
-         return elt & HASH_MASK2;
+      if (BJ64[elt_low] == bj)
+         return elt_low;
 
-      if ((elt & HASH_MASK1) == 0)
-         return HASH_NOT_FOUND;
-
-      elt &= HASH_MASK2;
-      do
-      {
-         if (BJ64[olist[elt] & HASH_MASK2] == bj)
-            return olist[elt] & HASH_MASK2;
-
-         elt = olist[elt];
-      } while ((elt & HASH_MASK1) == 0);
+      while (elt != elt_low) {
+         elt = olist[elt & HASH_MASK2];
+         elt_low = elt & HASH_MASK2;
+         if (BJ64[elt_low] == bj)
+            return elt_low;
+      }
 
       return HASH_NOT_FOUND;
    };
 
 private:
-   uint16_t  empty_slot;
+   /**
+    * `hsize` is the size of the hashtable (always a power of two).
+    * `hsize_minus1` is a convience constant for MOD hsize.
+    * `empty_slot` is the empty slot marker (e.g. nothing here)
+    *
+    * `BJ64` stores the inserted values
+    *
+    * `htable` tracks status of each slot:
+    *   low bits track where in BJ64 the associated value is held.
+    *   upper bit tracks if any item was moved (AKA "keep probing").
+    *
+    * `olist` [o]pen addressing list, when a conflict happens in slot s
+    *     store probing continue point, olist[j] = htable[s]
+    *     updates htable[s] = j
+    *
+    *     At lookup time if probing needs to continue, look in olist[s] to find the old htable[s]
+    *     Can keep doing elt = olist[elt] until you find an elemnt that doesn't need any more probing
+    */
    uint16_t *htable;
    uint16_t *olist;
+   uint64_t *BJ64;
    uint32_t  hsize;
    uint32_t  hsize_minus1;
-   uint64_t *BJ64;
+   uint16_t  empty_slot;
 };
 
 #endif
